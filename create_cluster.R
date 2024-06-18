65b5807ee9df5d93230e7190f6c1e6572c0dded9
Irene-liu-724 <yanjunirene@gmail.com>
2024-06-18 08:46:00 -0700

    Update create_cluster.R

--- create_cluster.R
+++ create_cluster.R
@@ -44,7 +44,66 @@ ext.cluster = function(x){
     distinct(row, col, .keep_all = TRUE)
   return(ext)
 }
-
+#-----------------------------------------------------------------------------
+#Plot hexagonal grid
+hexagon_grid = function(x){  #x is a data frame
+  #for a right triangle, a - shortest leg, b - the other leg, c - hypotenuse
+  b = tan(pi/3) 
+  a = 1 
+  c = sqrt(a^2 + b^2)
+  #set up coordinates for six vertexes and center point
+  v1 = c(0, b)
+  v2 = c(a, 2*b)
+  v3 = c(a+c, 2*b)
+  v4 = c(2*c, b)
+  v5 = c(a+c, 0)
+  v6 = c(a, 0)
+  center = c(c, b)
+  
+  dat = data.frame()
+  for (i in seq_len(nrow(x))){
+    id = factor(i)
+    row = x[i,]$row
+    col = x[i,]$col
+    #cluster = x[i,]$cluster
+    time = x[i,]$time
+    
+    #the position of hexagons depend on whether its column number is even or odd
+    if (col %% 2 == 1){
+      x_shift = 3*(col - 1)/2
+      y_shift = 2*(row - 1)
+      vertex_x = c(v1[1], v2[1], v3[1], v4[1], v5[1], v6[1]) + center[1]*x_shift
+      vertex_y = c(v1[2], v2[2], v3[2], v4[2], v5[2], v6[2]) + center[2]*y_shift
+      cpoint_x = center[1] + center[1]*x_shift
+      cpoint_y = center[2] + center[2]*y_shift
+    } 
+    
+    if (col %% 2 == 0){
+      x_shift = (col - 1)
+      y_shift = 2*row - 1
+      vertex_x = c(v1[1], v2[1], v3[1], v4[1], v5[1], v6[1]) + (center[1]+0.5*c)*x_shift
+      vertex_y = c(v1[2], v2[2], v3[2], v4[2], v5[2], v6[2]) + center[2]*y_shift
+      cpoint_x = center[1] + (center[1]+0.5*c)*x_shift
+      cpoint_y = center[2] + center[2]*y_shift
+    }
+    
+    values = data.frame(
+      ID = rep(id, each = 6),
+      #cluster = rep(cluster, each = 6),
+      time = rep(time, each = 6),
+      row = rep(x[i,]$row, each = 6),
+      col = rep(x[i,]$col, each = 6),
+      density = rep(x[i,]$density, each = 6),
+      vx = vertex_x,
+      vy = vertex_y,
+      cpointx = rep(cpoint_x, each = 6),
+      cpointy = rep(cpoint_y, each = 6)      )
+    
+    dat = rbind(dat, values)
+  }
+  
+  return(dat)
+}
 
 
 
